c7e1af8113c4e8671b080574e41403fe0766ca0f
risto <risto.kowaczewski@gmail.com>
2024-07-02 10:43:44 +0200

    DateTime Fix

--- src/components/elements/misc/ChangeTimeIcon.js
+++ src/components/elements/misc/ChangeTimeIcon.js
@@ -5,13 +5,21 @@ import TimeDifference from "../datepickers/TimeDifference";
 
 export function ChangeTimeIcon({field, metadata}) {
 
-    const isoDate = new Date(metadata[0]*1000).toISOString()
+    try {
+        const isoDate = new Date(metadata[0] * 1000).toISOString()
 
-    return <PopOverIconButton style={{cursor: "pointer", display: "flex"}} icon={<TimeDifference date={isoDate}/>} singleIcon={true}>
-        <div style={{padding: 20}}>
-            <div><b>Field</b>:<br/> {field}</div>
-            {metadata[0] && <div style={{marginTop: 15}}><b>Changed</b>: <br/><DateValue date={isoDate}/></div>}
-            {metadata[1] && <div style={{marginTop: 10}}><b >Detail</b>: <br/> {metadata[1]}</div>}
-        </div>
-    </PopOverIconButton>
+        return <PopOverIconButton style={{cursor: "pointer", display: "flex"}} icon={<TimeDifference date={isoDate}/>}
+                                  singleIcon={true}>
+            <div style={{padding: 20}}>
+                <div><b>Field</b>:<br/> {field}</div>
+                {metadata[0] && <div style={{marginTop: 15}}><b>Changed</b>: <br/><DateValue date={isoDate}/></div>}
+                {metadata[1] && <div style={{marginTop: 10}}><b>Detail</b>: <br/> {metadata[1]}</div>}
+            </div>
+        </PopOverIconButton>
+
+    } catch (e) {
+        return ""
+    }
+
+
 }
