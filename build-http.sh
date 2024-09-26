feeaf3036bce6e2ea33c07cb31f68431b1d2cbc8
risto <risto.kowaczewski@gmail.com>
2024-09-26 10:12:48 +0200

    1.0.3

--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > public/revision.txt
-docker build . --rm --no-cache -f http.Dockerfile -t tracardi/tracardi-gui:1.0.2
-docker push tracardi/tracardi-gui:1.0.2
+docker build . --rm --no-cache -f http.Dockerfile -t tracardi/tracardi-gui:1.0.3
+docker push tracardi/tracardi-gui:1.0.3
